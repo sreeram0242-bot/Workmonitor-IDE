b4fb4e014bfc51e2a552683b4fc093ee7d562466
sree ram <sreeram02420@gmail.com>
2026-07-24 20:21:27 +0530

    fix: PrismaClient typo

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -11,6 +11,6 @@ const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClientImpl({ adapter });
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
